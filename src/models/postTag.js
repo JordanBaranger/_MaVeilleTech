e15499f6c800d6182d289be762d760f177429599
Nimo <jordan.baranger@oclock.school>
2025-03-03 15:59:26 +0100

    améliorations model postTag

--- src/models/postTag.js
+++ src/models/postTag.js
@@ -4,27 +4,33 @@ import { DataTypes, Model } from "sequelize";
 export default class PostTag extends Model {}
 
 PostTag.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        tag: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    {
-        sequelize,
-        modelName: "PostTag",
-        tableName: "post_tag",
-        timestamps: true,
-        createdAt: "created_at",
-        updatedAt: "updated
-    }
+    tag: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "posts",
+        key: "id",
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+    },
+  },
+  {
+    sequelize,
+    modelName: "PostTag",
+    tableName: "post_tag",
+    timestamps: true,
+    createdAt: "created_at",
+    updatedAt: "updated_at",
+  }
 );
