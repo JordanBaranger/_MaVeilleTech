d888ad3a59ee9ed54e1fe333c056f90ee848eeab
Nimo <jordan.baranger@oclock.school>
2025-03-03 16:22:50 +0100

    améliorations model user

--- src/models/user.js
+++ src/models/user.js
@@ -11,28 +11,42 @@ User.init(
       primaryKey: true,
     },
     pseudo: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(100),
       allowNull: false,
+      unique: true,
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     role: {
-      type: DataTypes.STRING,
+      type: DataTypes.ENUM("admin", "user"),
+      defaultValue: "user",
       allowNull: false,
     },
     bio: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(255),
     },
     profilePicture: {
       type: DataTypes.STRING,
+      defaultValue: "default.png",
+      validate: {
+        isUrl: true,
+      },
     },
     is_active: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true,
+    },
+    is_deleted: {
       type: DataTypes.BOOLEAN,
       defaultValue: false,
     },
